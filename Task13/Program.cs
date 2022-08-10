165c8370424ba238c3f61c9a73051d650352585c
AYA <aya011294@gmail.com>
2022-08-10 22:21:22 +0600

    Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+
+int randomNumber = new Random().Next(10,999);
+Console.WriteLine(randomNumber);
+int a = 99;
+if (randomNumber < a)
+{
+Console.WriteLine( "третьей цифры нет");
+}
+else 
+{
+int number = randomNumber % 10;
+Console.WriteLine(number);
+}
