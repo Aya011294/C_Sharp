955fb9e0156583fd729699f0cb70cdd650d7b798
Aya <aya011294@gmail.com>
2022-08-23 20:40:19 +0600

    task29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -2,4 +2,24 @@
 5 -> [1, 2, 5, 7, 19]
 3 -> [6, 1, 33] */
 
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+void FillArrayRandomNumbers(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 20);
+    }
+}
 
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+} 
