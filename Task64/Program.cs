aad18ec1a196ffd2868fc79cecc3e939677503df
Aya <aya011294@gmail.com>
2022-09-08 01:36:48 +0600

    sep08.03

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -2,3 +2,28 @@
 // которая выведет все натуральные числа в промежутке от N до 1.
 // N = 5. -> "5, 4, 3, 2, 1"
 // N = 8. -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Write("Задайте число m: ");
+int m = int.Parse(Console.ReadLine());
+int n = 1;
+ 
+NaturalNumber(m, n);
+ 
+void NaturalNumber(int m, int n)
+{
+    if (m < n)
+    {
+        Console.Write($"{m}, не натуральное число");
+        // NaturalNumber(m + 1, n);
+    }
+    if (m > n)
+    {
+        Console.Write($"{m}, ");
+        NaturalNumber(m - 1, n);
+    }
+    if (m == n) 
+    {
+        Console.Write($"{m} ");
+    }
+
+}
--- Task66/Program.cs
+++ Task66/Program.cs
@@ -2,3 +2,18 @@
 // Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+Console.Write("Введите любое число: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write("Введите степень: ");
+int power = int.Parse(Console.ReadLine());
+ 
+int Power(int n, int pow)
+{
+    if(pow == 0)
+        return 1;
+
+    return n * Power(n, pow - 1);
+}
+ 
+Console.WriteLine($"{number} ^ {power} = " + Power(number, power));
